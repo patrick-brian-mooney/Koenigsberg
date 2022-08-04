ed887f3ad5f31a5824fb1486eedb9f53b724cca4
Patrick Mooney <github.pmooney78@neverbox.com>
2022-08-04 14:32:06 -0500

    4 Aug 2022: unit tests in tests/tests.py are working again!

--- tests/tests.py
+++ tests/tests.py
@@ -55,44 +55,44 @@ class TestDataSanityChecks(unittest.TestCase):
     def test_graph_sanity(self) -> None:
         with self.assertRaises(ValueError):
             # GRAPH must be a dict
-            koenigsberg._sanity_check_graph(graph=[1, 2, 3, 4])
+            kl._sanity_check_graph(graph=[1, 2, 3, 4])
             # GRAPH values must be an iterable
-            koenigsberg._sanity_check_graph(graph={1: 2})
+            kl._sanity_check_graph(graph={1: 2})
             # GRAPH values must not be strings or similar, even though those are iterable too
-            koenigsberg._sanity_check_graph(graph={1: "ABC"})
+            kl._sanity_check_graph(graph={1: "ABC"})
             # values in GRAPH destination lists must contain hashable objects
-            koenigsberg._sanity_check_graph(graph={1: (1, 2, 3, [4, 5, 6]), 2: (1, 3), 3: (1, 2)})
+            kl._sanity_check_graph(graph={1: (1, 2, 3, [4, 5, 6]), 2: (1, 3), 3: (1, 2)})
             # values in GRAPH destination list must also be nodes that occur in the keys of the same graph dictionary
-            koenigsberg._sanity_check_graph(graph={1: (2, 3, 4), 2: (1, 3), 3: (1, 2)})
+            kl._sanity_check_graph(graph={1: (2, 3, 4), 2: (1, 3), 3: (1, 2)})
             # graphs that map A -> B must also map B -> A
-            koenigsberg._sanity_check_graph(graph={1: (2, 3, 4), 2: (3, 4), 3: (1, 2), 4: (1, 2)})
+            kl._sanity_check_graph(graph={1: (2, 3, 4), 2: (3, 4), 3: (1, 2), 4: (1, 2)})
 
     def test_dict_pair_sanity(self) -> None:
         with self.assertRaises(ValueError):
             # PATHS_TO_NODES and NODES_TO_PATHS must be dicts
-            koenigsberg._sanity_check_dicts(paths_to_nodes=True, nodes_to_paths=dict())
-            koenigsberg._sanity_check_dicts(paths_to_nodes=dict(), nodes_to_paths=True)
+            kl._sanity_check_dicts(paths_to_nodes=True, nodes_to_paths=dict())
+            kl._sanity_check_dicts(paths_to_nodes=dict(), nodes_to_paths=True)
             # max possible paths is 255
-            koenigsberg._sanity_check_dicts(paths_to_nodes={k: True for k in range(1000)}, nodes_to_paths={})
+            kl._sanity_check_dicts(paths_to_nodes={k: True for k in range(1000)}, nodes_to_paths={})
             # all nodes mentioned as part of paths must be mentioned in keys of nodes dict
-            koenigsberg._sanity_check_dicts(paths_to_nodes={(1, 2): (1, 2), (1, 4): (1, 4), (2, 3): (2, 3), (3, 4): (3, 4)},
-                                            nodes_to_paths={1: ((1, 2), (1, 4)), 2: ((1, 2), (2, 3)), 4: ((1, 4), (3, 4))})
+            kl._sanity_check_dicts(paths_to_nodes={(1, 2): (1, 2), (1, 4): (1, 4), (2, 3): (2, 3), (3, 4): (3, 4)},
+                                   nodes_to_paths={1: ((1, 2), (1, 4)), 2: ((1, 2), (2, 3)), 4: ((1, 4), (3, 4))})
             # all nodes mentioned in keys of nodes dict must be mentioned in lists in path dict
-            koenigsberg._sanity_check_dicts(paths_to_nodes={(2, 3): (2, 3), (3, 4): (3, 4)},
-                                            nodes_to_paths={1: ((1, 2), (1, 4)), 2: ((1, 2), (2, 3)), 3: ((2, 3), (3, 4)), 4: ((1, 4), (3, 4))})
+            kl._sanity_check_dicts(paths_to_nodes={(2, 3): (2, 3), (3, 4): (3, 4)},
+                                   nodes_to_paths={1: ((1, 2), (1, 4)), 2: ((1, 2), (2, 3)), 3: ((2, 3), (3, 4)), 4: ((1, 4), (3, 4))})
             # node lists in paths_to_nodes must be iterable -- probably inevitably caught by previous rules, but check explicitly
-            koenigsberg._sanity_check_dicts(paths_to_nodes={'hello': True, 'there': (1, 2)}, nodes_to_paths={True: (1, 2, 'hello'), (1, 2, 3): False})
+            kl._sanity_check_dicts(paths_to_nodes={'hello': True, 'there': (1, 2)}, nodes_to_paths={True: (1, 2, 'hello'), (1, 2, 3): False})
             # paths in path-to-node node lists must connect exactly two nodes
-            koenigsberg._sanity_check_dicts(paths_to_nodes={'hello': (1, 2), 'there': (1, 2, 3)}, nodes_to_paths={1: ('hello'), 2: ('hello', 'there'), 3: ('there')})
+            kl._sanity_check_dicts(paths_to_nodes={'hello': (1, 2), 'there': (1, 2, 3)}, nodes_to_paths={1: ('hello'), 2: ('hello', 'there'), 3: ('there')})
             # nodes mentioned in PATHS_TO_NODES list must also appear as keys in NODES_TO_PATHS
-            koenigsberg._sanity_check_dicts(paths_to_nodes={'hello': (1, 2), 'there': (2, 3), 'young': (3, 4), 'fellow': (1, 3)},
-                                            nodes_to_paths={1: ('hello', 'fellow'), 2: ('hello', 'there'), 3: ('there', 'young', 'fellow')})
+            kl._sanity_check_dicts(paths_to_nodes={'hello': (1, 2), 'there': (2, 3), 'young': (3, 4), 'fellow': (1, 3)},
+                                   nodes_to_paths={1: ('hello', 'fellow'), 2: ('hello', 'there'), 3: ('there', 'young', 'fellow')})
             # path lists that occur in the values of NODES_TO_PATHS must be iterable.
-            koenigsberg._sanity_check_dicts(paths_to_nodes={'hello': (1, 2), 'there': (2, 3), 'young': (3, 4), 'fellow': (1, 3)},
-                                            nodes_to_paths={1: ('hello', 'fellow'), 2: ('hello', 'there'), 3: ('there', 'young', 'fellow'), 4: None})
+            kl._sanity_check_dicts(paths_to_nodes={'hello': (1, 2), 'there': (2, 3), 'young': (3, 4), 'fellow': (1, 3)},
+                                   nodes_to_paths={1: ('hello', 'fellow'), 2: ('hello', 'there'), 3: ('there', 'young', 'fellow'), 4: None})
             # paths appearing in the values of NODES_TO_PATHS must only contain paths that appear as keys in paths_to_nodes
-            koenigsberg._sanity_check_dicts(paths_to_nodes={(1, 2): (1, 2), (1, 4): (1, 4), (2, 3): (2, 3)},
-                                            nodes_to_paths={1: ((1, 2), (1, 4)), 2: ((1, 2), (2, 3)), 3: ((2, 3), (3, 4)), 4: ((1, 4), (3, 4))})
+            kl._sanity_check_dicts(paths_to_nodes={(1, 2): (1, 2), (1, 4): (1, 4), (2, 3): (2, 3)},
+                                   nodes_to_paths={1: ((1, 2), (1, 4)), 2: ((1, 2), (2, 3)), 3: ((2, 3), (3, 4)), 4: ((1, 4), (3, 4))})
 
 
 if __name__ == "__main__":
